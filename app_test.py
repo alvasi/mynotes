4007d7b17d4571384500a9f37fd4233c6fc6da50
Alva Si <alva.si19@imperial.ac.uk>
2024-03-03 16:29:42 +0000

    add python formatting test

--- app_test.py
+++ app_test.py
@@ -7,6 +7,7 @@ from flask import json
 # Load environment variables from .env file
 load_dotenv()
 
+
 class TestDatabaseConnection(unittest.TestCase):
     def test_db_connection(self):
         """Test database connection."""
@@ -24,18 +25,18 @@ class TestDatabaseConnection(unittest.TestCase):
                 connection.close()
                 print("Database connection closed.")
 
+
 class FlaskTestCase(unittest.TestCase):
 
     # This will run before every test
     def setUp(self):
         # Set up the app to use the testing configuration
-        app.config['TESTING'] = True
+        app.config["TESTING"] = True
         self.app = app.test_client()
-
 
     def test_all_deadlines(self):
         # Simulate a request to the /all_deadlines endpoint
-        response = self.app.get('/all_deadlines?username=testuser')
+        response = self.app.get("/all_deadlines?username=testuser")
 
         # Check if the response is OK
         self.assertEqual(response.status_code, 200)
@@ -43,9 +44,8 @@ class FlaskTestCase(unittest.TestCase):
         # Optionally, check if the data returned is correct
         data = json.loads(response.data)
         self.assertIsInstance(data, dict)
-        self.assertIn('entries', data)
+        self.assertIn("entries", data)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
